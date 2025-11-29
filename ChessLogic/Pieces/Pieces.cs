d45b315457d8635ddb90d0851e759d8e13f2dd33
khafifi <khafifiayman318@gmail.com>
2025-11-29 22:20:48 +0100

    Third commit

--- ChessLogic/Pieces/Pieces.cs
+++ ChessLogic/Pieces/Pieces.cs
@@ -13,5 +13,31 @@ namespace ChessLogic
         public bool HasMoved { get; set; } = false;
         public abstract Pieces Copy();
 
+        public abstract IEnumerable<Move> GetMoves(Position from, Board board);
+
+        protected IEnumerable<Position> MovePositionInDir(Position from, Board board, Direction dir)
+        {
+            for(Position pos=from+dir; Board.IsInside(pos); pos+=dir)
+            {
+                if(board.IsEmpty(pos))
+                {
+                    yield return pos;
+                    continue;
+                }
+
+                Pieces pieces = board[pos];
+                if(pieces.Color!=Color)
+                {
+                    yield return pos;
+                }
+
+                yield break;
+            }
+        }
+
+        protected IEnumerable<Position> MovePositionInDirs(Position from, Board board, Direction[] dirs)
+        {
+            return dirs.SelectMany(dir => MovePositionInDir(from, board, dir));
+        }
     }
 }
--- ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
+++ ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09076fae2fac84dfd3e0ce6e52b50a7d5c03a00e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1259c54f9ae978947a6c31464971020e3b3d28d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
